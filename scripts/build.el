5279a0581c5aedc48d8cd882e3a983a0d86491bc
alexforsale <alexforsale@yahoo.com>
2024-02-22 11:35:39 +0700

    github-action scripts

---
+++ scripts/build.el
@@ -0,0 +1,13 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-refresh-contents)
+(package-initialize)
+(package-install 'ox-hugo)
+(require 'ox-hugo)
+
+(defun export-org-files ()
+  "Export files to hugo markdown."
+  (interactive)
+  (dolist (f (append (file-expand-wildcards "content-org/**/*.org")))
+    (with-current-buffer (find-file f)
+      (org-hugo-export-to-md))))
